5808c1b9308894d151587bcdd65d81088fd298bb
benj=lazaro <benjie.work@gmail.com>
2022-01-17 21:05:35 +0800

    Refactor variable name

--- day-01-band-name-generator/band-name-generator.py
+++ day-01-band-name-generator/band-name-generator.py
@@ -2,8 +2,8 @@
 print("Welcome to the Band Name Generator.")
 
 # Get input from the user
-user_city_name = input("What's the name of the city you grew up in?\n")
+user_name_of_city = input("What's the name of the city you grew up in?\n")
 user_name_of_pet = input("What's your pet's name?\n")
 
 # Concatenate user input into a string & then print Band Name on console / command-line / terminal
-print("Your band name could be " + user_city_name + " " + user_name_of_pet + "\n")
+print("Your band name could be " + user_name_of_city + " " + user_name_of_pet + "\n")
