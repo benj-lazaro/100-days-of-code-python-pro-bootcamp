2b3630087fedd6aa9def74a2c7580eed104893ca
benj=lazaro <benjie.work@gmail.com>
2022-01-21 22:15:37 +0800

    Day 05 PyPassword Generator 'Easy level' commit

---
+++ day-05-pypassword-generator/pypassword.py
@@ -0,0 +1,31 @@
+# Import random module
+import random
+
+# List of lowercae & uppercase letters, numbers & symbols
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Display greetings
+print("Welcome to the PyPassword Generator!")
+
+# Ask user how many letters, symbols & numbers to be included in the password
+password_letters = int(input("How many letters would you like in your password? "))
+password_symbols = int(input("How many symbols would you like? "))
+password_numbers = int(input("How many numbers? "))
+
+password_generated = ""
+# Easy level: Generate password in sequence order (letter, symbols & number)
+for character in range(1, password_letters + 1):
+    password_generated += random.choice(letters)
+
+for symbol in range(1, password_symbols + 1):
+    password_generated += random.choice(symbols)
+
+for number in range(1, password_numbers + 1):
+    password_generated += random.choice(numbers)
+
+# Display generated password
+print(password_generated)
+
+# Hard level: Generate password in completely random order
---
+++ day-05-pypassword-generator/readme.md
@@ -0,0 +1,23 @@
+## PyPassword Generator
+A command-line password generator
+
+### What does it do?
+The program will ask how many:
+* Letters
+* Symbols
+* Numbers
+
+That will be included on their password to be generated
+
+### How does it work?
+On the command-line (or terminal), type the following:<br>
+python3 <b>pypassword-generator.py</b>
+
+The program will prompt the user a number of questions, answers are typed followed by pressing the <b>ENTER</b> key.
+
+<b>NOTE:</b> It is assumed that the <b>Python 3 interpreter</b> has already been installed on your computer.
+
+### Program Recipe:
+The program is created using <b>random</b> module, <b>random.choice()</b> method, <b>for-in</b> loop, <b>string concatenation</b>, <b>data type conversion</b> and the following <b>built-in Python functions</b>:
+* <b>input()</b>
+* <b>print()</b>
