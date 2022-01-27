d4965906328beb3a285bdf3d4d580f0a81033c6a
benj=lazaro <benjie.work@gmail.com>
2022-01-27 16:22:03 +0800

    Refactor 1st step code

--- day-07-hangman/hangman.py
+++ day-07-hangman/hangman.py
@@ -5,16 +5,15 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 
 # TODO #1: Randomly choose a word from the word_list and assign it to a variable called chosen_word
-random_index = random.randint(0, len(word_list) - 1)
-chosen_word = word_list[random_index]
+chosen_word = random.choice(word_list)
 
 # TODO #2: Ask the user to guess a letter and assign their answer to a variable called guess.
 # Make the value of guess lowercase.
 guess = str(input("Guess a letter: ")).lower()
 
 # TODO #3: Check if the letter the user guessed (guess) is one of the letters in the chosen_word
-for character in range(0, len(chosen_word)):
-    if (chosen_word[character] == guess):
+for character in chosen_word:
+    if character == guess:
         print("Right")
     else:
         print("Wrong")
