c1326c684234c504b332772572f8256bb322545c
benj=lazaro <benjie.work@gmail.com>
2022-02-01 16:18:04 +0800

    4th step commit

--- day-07-hangman/hangman.py
+++ day-07-hangman/hangman.py
@@ -1,10 +1,74 @@
-# Step 3
+# Step 4
 
 import random
+
+# Different stages of the hangman
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 chosen_word_length = len(chosen_word)
 end_of_game = False
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+# Set 'lives' to equal 6.
+
+lives = 6
 
 # Testing code
 print(f"Psst, the solution is {chosen_word}.")
@@ -19,11 +83,30 @@ while not end_of_game:
 
     # Check guessed letter
     for index_position in range(chosen_word_length):
-        if guess == chosen_word[index_position]:
-            display[index_position] = guess
+        letter = chosen_word[index_position]
 
-    print(display)
+        if letter == guess:
+            display[index_position] = guess
+            matched_letter = guess
+
+# TODO-2: - If guess is not a letter in the chosen_word,
+# Then reduce 'lives' by 1.
+# If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    if matched_letter != guess:
+        lives -= 1
+
+    if lives == 0:
+        print("Sorry you lose.")
+        end_of_game = True
+
+# Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+ # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
+    print(stages[lives])
