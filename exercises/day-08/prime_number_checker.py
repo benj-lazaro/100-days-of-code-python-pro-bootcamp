d0919b91e4d253b8d027c308b195016b4e1b9487
benj=lazaro <benjie.work@gmail.com>
2022-02-02 16:06:03 +0800

    Day 08 exercise code refactor

--- exercises/day-08/prime_number_checker.py
+++ exercises/day-08/prime_number_checker.py
@@ -1,8 +1,12 @@
 #Write your code below this line 👇
 def  prime_checker(number):
-    if (number == 1 ) or (number == 2):
-        print("It's a prime number.")
-    elif (number % 2 != 0) and (number % 3 != 0):
+    is_prime = True
+
+    for x in range(2, number):
+        if (number % x == 0):
+            is_prime = False
+
+    if is_prime:
         print("It's a prime number.")
     else:
         print("It's not a prime number.")
