9cece44c067a662ef707f1dbb42c87890c1abaa4
benj=lazaro <benjie.work@gmail.com>
2022-02-07 11:37:55 +0800

    2nd step code commit

--- day-08-caesar-cipher/caesar_cipher.py
+++ day-08-caesar-cipher/caesar_cipher.py
@@ -4,7 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def  encrypt(plain_text, shift_value):
     cipher_text = ""
 
@@ -22,17 +21,47 @@ def  encrypt(plain_text, shift_value):
 
     print(f"The encoded text is {cipher_text}")
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def  decrypt(cipher_text, shift_value):
+    clear_text = ""
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    for letter in cipher_text:
+        cipher_index = alphabet.index(letter)
+        clear_letter_index = cipher_index - shift_value
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+        if (cipher_index <= 0):
+            cipher_index = 25
+            clear_letter_index = cipher_index - shift_value + 1
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-encrypt(plain_text=text, shift_value=shift)
+        clear_letter = alphabet[clear_letter_index]
+        clear_text += clear_letter
+
+    print(f"The decoded text is {clear_text}")
+
+    # for letter in cipher_text:
+    #     cipher_index = alphabet.index(letter)
+    #     clear_letter_index = cipher_index - 1
+    #
+    #     if (cipher_index <= 0):
+    #         cipher_index = 25
+    #         clear_letter_index = cipher_index - shift_value + 1
+    #
+    #     clear_letter = alphabet[clear_letter_index]
+    #     clear_text += clear_letter
+    #
+    # print(f"The decoded text is {clear_text}")
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+  #e.g.
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
+if (direction.lower() == "encode"):
+    encrypt(plain_text=text, shift_value=shift)
+else:
+    decrypt(cipher_text=text, shift_value=shift)
