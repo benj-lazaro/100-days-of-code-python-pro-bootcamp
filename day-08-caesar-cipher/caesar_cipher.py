b02cd0392708c5c860d603d68095da7237e4561b
benj=lazaro <benjie.work@gmail.com>
2022-02-07 16:22:14 +0800

    Refactor 2nd step code comments

--- day-08-caesar-cipher/caesar_cipher.py
+++ day-08-caesar-cipher/caesar_cipher.py
@@ -30,6 +30,14 @@ def  encrypt(plain_text, shift_value):
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 def  decrypt(cipher_text, shift_value):
+
+      #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+      #e.g.
+      #cipher_text = "mjqqt"
+      #shift = 5
+      #plain_text = "hello"
+      #print output: "The decoded text is hello"
+
     clear_text = ""
 
     for letter in cipher_text:
@@ -40,14 +48,6 @@ def  decrypt(cipher_text, shift_value):
         clear_text += clear_letter
 
     print(f"The decoded text is {clear_text}")
-
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
-  #e.g.
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
