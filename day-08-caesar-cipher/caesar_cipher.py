415096b188fc30d3f8003fb3dc301b24c066358a
benj=lazaro <benjie.work@gmail.com>
2022-02-11 14:29:33 +0800

    4th step code commit

--- day-08-caesar-cipher/caesar_cipher.py
+++ day-08-caesar-cipher/caesar_cipher.py
@@ -1,10 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 def  caesar(text, shift, direction):
     max_index = 25
     end_text = ""
@@ -19,5 +14,31 @@ def  caesar(text, shift, direction):
 
     print(f"The {direction}d text is {end_text}")
 
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-caesar(text, shift, direction)
+# TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+
+print(logo)
+
+# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+
+run_program = True
+
+while run_program:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# Try running the program and entering a shift number of 45.
+# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+# Hint: Think about how you can use the modulus (%).
+
+    caesar(text, shift, direction)
+
+    continue_program = input("Do you want to restart the cipher program? [yes/no]: ").lower()
+
+    if continue_program == "no":
+        run_program = False
