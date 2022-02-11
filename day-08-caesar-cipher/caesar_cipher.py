13a94393575c5401a64c753ede529e4b1d0051b8
benj=lazaro <benjie.work@gmail.com>
2022-02-11 15:18:10 +0800

    Refactor 4th step code

--- day-08-caesar-cipher/caesar_cipher.py
+++ day-08-caesar-cipher/caesar_cipher.py
@@ -7,23 +7,29 @@ def  caesar(text, shift, direction):
     if direction == "decode":
         shift *= -1
 
+# TODO-3: What happens if the user enters a number/symbol/space?
+# Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+# e.g. start_text = "meet me at 3"
+# end_text = "•••• •• •• 3"
+
     for character in text:
-        current_position = alphabet.index(character)
-        new_position = current_position + shift
-        end_text += alphabet[new_position]
+        if character in alphabet:
+            current_position = alphabet.index(character)
+            new_position = current_position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += character
 
     print(f"The {direction}d text is {end_text}")
 
 # TODO-1: Import and print the logo from art.py when the program starts.
 from art import logo
-
 print(logo)
 
 # TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 # e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 # If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 # Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-
 run_program = True
 
 while run_program:
@@ -35,10 +41,11 @@ while run_program:
 # Try running the program and entering a shift number of 45.
 # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
 # Hint: Think about how you can use the modulus (%).
+    shift %= 26
 
     caesar(text, shift, direction)
 
-    continue_program = input("Do you want to restart the cipher program? [yes/no]: ").lower()
-
+    continue_program = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
     if continue_program == "no":
         run_program = False
+        print("Goodbye")
