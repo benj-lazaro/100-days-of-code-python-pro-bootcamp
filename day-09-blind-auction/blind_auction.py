795c2d425ec5ff10618ca3bb664589140649c304
benj=lazaro <benjie.work@gmail.com>
2022-02-14 16:53:00 +0800

    Refactor day 09 Blind Auction code

--- day-09-blind-auction/blind_auction.py
+++ day-09-blind-auction/blind_auction.py
@@ -1,23 +1,31 @@
 # Import art.py for program logo
 from art import logo
 
-# Global data structure
+# Global data structure & variable
 bidders = {}
-
-# Global variables
 auction = True
-highest_bid = 0
-highest_bidder = ""
 
 # Mimic replit's clear() function
 def clear_terminal_screen():
     print("\n" * 100)
 
+# Determine the auction's winner (i.e. highest bidder)
+def auction_winner():
+    highest_bid = 0
+    highest_bidder = ""
+
+    for key in bidders:
+        if bidders[key] > highest_bid:
+            highest_bidder = key
+            highest_bid = bidders[key]
+
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}.")
+
 # Print logo & welcome message
 print(logo)
 print("Welcome to the secret auction program.")
 
-# Gather bidder names & bid values
+# Gather data (name & bid value) & then determine the winnder
 while auction:
     name = input("What is your name?: ")
     bid_value = int(input("What's your bid?: $"))
@@ -29,11 +37,4 @@ while auction:
     else:
         auction = False
         clear_terminal_screen()
-
-# Determine the auction's winner (i.e. highest bidder)
-for key in bidders:
-    if bidders[key] > highest_bid:
-        highest_bidder = key
-        highest_bid = bidders[key]
-
-print(f"The winner is {highest_bidder} with a bid of ${highest_bid}.")
+        auction_winner()
