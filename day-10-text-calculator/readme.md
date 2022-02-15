557b74bd046eadf4f2681a189a685690d4e230fc
benj=lazaro <benjie.work@gmail.com>
2022-02-15 18:03:49 +0800

    Day 10 Text Calculator 1st step code commit

---
+++ day-10-text-calculator/readme.md
@@ -0,0 +1,18 @@
+## Text Calculator
+A command-line text Calculator
+
+### What does it do?
+The program will ask the user to
+* Provide the first number
+* Select the preferred mathematical operation (add, subtract, multiply or divide)
+* Provide the next number then display the corresponding mathematical expression with the result
+* Asks if it will continue calculating with the result or start a new calculation
+
+### How does it work?
+On the command-line (or terminal), type the following:<br>
+python3 <b>text_calculator.py</b>
+
+<b>NOTE:</b> It is assumed that the <b>Python 3 interpreter</b> has already been installed on your computer.
+
+### Program Recipe:
+The program is created using <b>for-in</b> & <b>while</b> loops, <b>data type conversion</b>, <b>dictionary</b> data structure, <b>if-else</b> statement and function
---
+++ day-10-text-calculator/text_calculator.py
@@ -0,0 +1,39 @@
+
+# Addition
+def add(number1, number2):
+    """Adds two numerical inputs & returns the sum."""
+    return number1 + number2
+
+# Subtraction
+def subtract(number1, number2):
+    """Subtracts two numerical inputs & returns the difference."""
+    return number1 - number2
+
+# Multiplication
+def multiply(number1, number2):
+    """Multiples two numerical inputs & returns the product."""
+    return number1 * number2
+
+# Division
+def divide(number1, number2):
+    """Divides two numerical inputs & returns the quotient."""
+    return number1 / number2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
