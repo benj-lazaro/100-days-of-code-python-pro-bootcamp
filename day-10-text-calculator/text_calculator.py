e0dd2382533d9977ee22e3b9182b79446d3a4c51
benj=lazaro <benjie.work@gmail.com>
2022-02-16 21:59:47 +0800

    Day 10 Text Calculator 2nd step code commit

--- day-10-text-calculator/text_calculator.py
+++ day-10-text-calculator/text_calculator.py
@@ -35,5 +35,11 @@ operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number?: "))
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+first_answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
