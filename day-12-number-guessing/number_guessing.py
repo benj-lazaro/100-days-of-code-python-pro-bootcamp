b50c42d3eab1bc82716858ba1197ccb63a4e537c
benj-lazaro <benjie.work@gmail.com>
2022-02-28 23:11:54 +0800

    Day 12 Number Guessing Game initial commit

---
+++ day-12-number-guessing/number_guessing.py
@@ -0,0 +1,68 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+import random
+
+# Game difficulty level
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
+number_of_guesses = 0
+is_game_over = False
+
+def clear_screen_terminal():
+    """Clears the terminal screen."""
+    print("\n" * 100)
+
+def compare(user_guess, random_number):
+    """Takes in two integers, compare the values & return corresponding result"""
+    if user_guess > random_number:
+        return "Too high."
+    elif user_guess < random_number:
+        return "Too low."
+    else:
+        return 0
+
+clear_screen_terminal()
+
+print("Welcome to the Number Guessing Game!\n")
+print("I'm thinking of a number between 1 and 100.\n")
+
+# Get preference on game difficulty level
+difficulty_level = str(input("Choose a difficulty level. Type 'easy' or 'hard': "))
+
+if difficulty_level == 'easy':
+    number_of_guesses = EASY_LEVEL
+    print(f"\nYou will have {number_of_guesses} attempts remaining to guess the number.")
+else:
+    number_of_guesses = HARD_LEVEL
+    print(f"\nYou will have {number_of_guesses} attempts remaining to guess the number.")
+
+# Generate random number between 1 and 100
+random_number = random.randint(1, 100)
+
+# Play the game
+while not is_game_over:
+    user_guess = int(input("Make a guess: "))
+    result = compare(user_guess, random_number)
+
+    if result == 0:
+        is_game_over = True
+        print(f"You got it! The answer is {random_number}.")
+    else:
+        print(result)
+
+    number_of_guesses -= 1
+
+    if number_of_guesses == 0:
+        is_game_over = True
+        print("You've run out of guesses, you lose.")
+    elif number_of_guesses != 0 and not is_game_over:
+        print(f"You have {number_of_guesses} attempts remaining to guess the number.")
---
+++ day-12-number-guessing/readme.md
@@ -0,0 +1,14 @@
+## Number Guessing
+A command-line Number Guessing game
+
+### What does it do?
+The program will:
+* Ask the player to guess a number between 1 and 100
+* Ask the player the level of difficulty: 'easy' (10 attempts) or 'hard' (5 attempts)
+* Show the answer if the user guessed correctly
+
+### How does it work?
+On the command-line (or terminal), type the following:<br>
+python3 <b>number_guessing.py</b>
+
+<b>NOTE:</b> It is assumed that the <b>Python 3 interpreter</b> has already been installed on your computer.
