db37ff3abab9d8961064eb484f24cc53c494ab66
benj-lazaro <benjie.work@gmail.com>
2022-03-02 17:52:33 +0800

    Refactor code comments

--- day-12-number-guessing/number_guessing.py
+++ day-12-number-guessing/number_guessing.py
@@ -10,6 +10,7 @@
 
 from random import randint
 
+# Number of turns based on chosen difficulty level
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 
@@ -21,12 +22,13 @@ def clear_screen_terminal():
     print("\n" * 100)
 
 def check_answer(user_guess, answer):
-    """Takes in two integers, compare the values & return corresponding feedback"""
+    """Takes in two integers, compare them & return corresponding feedback"""
     if user_guess > answer:
         return "Too high."
     elif user_guess < answer:
         return "Too low."
     else:
+        # Both integers are equal i.e guessed correctly
         return 0
 
 def set_game_level(level):
@@ -36,19 +38,20 @@ def set_game_level(level):
     else:
         return HARD_LEVEL
 
+# Welcome message
 clear_screen_terminal()
-
 print("Welcome to the Number Guessing Game!\n")
 print("I'm thinking of a number between 1 and 100.\n")
 
 # Generate random number between 1 and 100
 answer = randint(1, 100)
 
-# Get preference on game difficulty level
+# Set game difficulty level
 difficulty_level = str(input("Choose a difficulty level. Type 'easy' or 'hard': "))
 number_of_turns = set_game_level(difficulty_level)
 print(f"\nYou will have {number_of_turns} attempts remaining to guess the number.")
 
+# Loop until the number has been guessed right or ran out of turns
 while not is_game_over:
     user_guess = int(input("Make a guess: "))
     result = check_answer(user_guess, answer)
@@ -57,6 +60,7 @@ while not is_game_over:
         is_game_over = True
         print(f"You got it! The answer is {answer}.")
     else:
+        # Display corresponding feedback to user
         print(result)
 
     number_of_turns -= 1
