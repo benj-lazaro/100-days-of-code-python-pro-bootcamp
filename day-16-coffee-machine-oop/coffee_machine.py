32f5dd442b7ac4a329758118675ed8fb8af9f2db
benj-lazaro <benjie.work@gmail.com>
2022-03-13 17:34:39 +0800

    Day 16 Coffee Machine code refactor to address logical bug

--- day-16-coffee-machine-oop/coffee_machine.py
+++ day-16-coffee-machine-oop/coffee_machine.py
@@ -16,22 +16,21 @@ while is_coffee_machine_on:
     # Get user's order
     order = str(input(f"What would you like? ({menu.get_items()}): ")).lower()
 
-    # Check for existence of preferred hot beverage
-    coffee_order = menu.find_drink(order)
-
-    # If ordered beverage is available
-    if coffee_order is not None:
-        # Check if resources are available
-        if coffee_maker.is_resource_sufficient(coffee_order):
-            # Check if payment is sufficient
-            if cash_register.make_payment(coffee_order.cost):
-                # Make the ordered beverage
-                coffee_maker.make_coffee(coffee_order)
-    # Otherwise, check for hidden maintenance commands: off & report
-    elif order == "off":
-        # Turn off coffee maker
+    # Check for hidden maintenance commands
+    if order == "off":
         is_coffee_machine_on = False
     elif order == "report":
-        # Report on available resource & current profit
         coffee_maker.report()
         cash_register.report()
+    else:
+        # Take coffee order
+        coffee_order = menu.find_drink(order)
+
+        # If order is available
+        if coffee_order is not None:
+            # Check if resources is available
+            if coffee_maker.is_resource_sufficient(coffee_order):
+                # Check if payment is sufficient
+                if cash_register.make_payment(coffee_order.cost):
+                    # Prepare the ordered hot beverage
+                    coffee_maker.make_coffee(coffee_order)
