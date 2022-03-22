42daa00a449e9f0b1a202a9876e13762789bf0fc
benj-lazaro <benjie.work@gmail.com>
2022-03-22 18:06:31 +0800

    Day 20 Snake Game 2nd step commit.

--- day-20-snake-game/snake_game.py
+++ day-20-snake-game/snake_game.py
@@ -1,21 +1,29 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 # Instantiate object from class Screen
 screen = Screen()
 
-# Setup screen as 600 pixels wide by 600 pixels high
+# Setup screen as 600 pixels wide by 600 pixels high, black background color & title of "My Snake Game"
 screen.setup(width=600, height=600)
-
-# Set screen background color to black and title to "My Snake Game"
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-# X-Y axis coordinates of 3-segment starter snake
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
+# Call and turn off the tracer() method of the Screen class
+screen.tracer(0)
 
-for position in starting_position:
-    new_turtle = Turtle("square")
-    new_turtle.color("white")
-    new_turtle.goto(position)
+# Instantiate an object from Snake class
+snake = Snake()
+
+game_is_on = True
+
+while game_is_on:
+    # Updates the screen to display; moving  the 3-segment starter snake body as a single piece
+    screen.update()
+    # Update the screen every 0.1 seconds
+    time.sleep(0.1)
+
+    snake.move()
 
 screen.exitonclick()
