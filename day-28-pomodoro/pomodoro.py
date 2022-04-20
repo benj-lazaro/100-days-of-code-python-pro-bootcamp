23b8a8f42554ee2cf9e7a3b4f8c0fd790dbf20b8
benj-lazaro <benjie.work@gmail.com>
2022-04-20 17:53:55 +0800

    Day 28 Pomodoro app GUI layout commit

--- day-28-pomodoro/pomodoro.py
+++ day-28-pomodoro/pomodoro.py
@@ -21,13 +21,29 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# Add a Canvass widget
+# Add a timer label widget
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 32, "bold"))
+timer_label.grid(column=1, row=0)
+
+# Add a canvass widget
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # Add the tomato image at the center of the canvas
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 # Add some text over the tomato image
 canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Add a start button widget
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+# Add a reset button widget
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+# Add a check label widget
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_label.grid(column=1, row=3)
 
 window.mainloop()
