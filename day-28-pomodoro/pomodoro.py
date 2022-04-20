fcbf03b85c4c6f5fba9eb2858b9607031ce58129
benj-lazaro <benjie.work@gmail.com>
2022-04-20 21:48:35 +0800

    Day 28 Pomodoro app GUI layout refactor

--- day-28-pomodoro/pomodoro.py
+++ day-28-pomodoro/pomodoro.py
@@ -22,8 +22,8 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 # Add a timer label widget
-timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 32, "bold"))
-timer_label.grid(column=1, row=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
 
 # Add a canvass widget
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -35,15 +35,15 @@ canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 28, "b
 canvas.grid(column=1, row=1)
 
 # Add a start button widget
-start_button = Button(text="Start")
+start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
 
 # Add a reset button widget
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-# Add a check label widget
-check_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
-check_label.grid(column=1, row=3)
+# Add a check mark widget
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
