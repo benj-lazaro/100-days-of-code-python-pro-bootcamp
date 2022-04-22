4ee3ab0e2dad252bc5965b2a1d5fb5759828fc84
benj-lazaro <benjie.work@gmail.com>
2022-04-22 16:16:30 +0800

    Day 28 Pomodoro app; updates the label according to the time "mode" currently running

--- day-28-pomodoro/pomodoro.py
+++ day-28-pomodoro/pomodoro.py
@@ -7,17 +7,38 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+
+# ---------------------------- GLOBAL VARIABLES ------------------------------- #
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    """Simply calls the count_down() function; counts as minutes"""
-    count_down(5 * 60)
+    """Calls the count_down() & pass the corresponding time duration values"""
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it is the 8th repetition, take a long break
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    # Otherwise, take a short break
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    # Back to work
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -35,9 +56,13 @@ def count_down(count):
     # Display the countdown timer in the minutes:seconds format
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
 
+    # To prevent from counting down to the negative value
     if count > 0:
         # Calls itself after 1 second, pass count - 1 as parameter & loop through the function itself
         window.after(1000, count_down, count - 1)
+    else:
+        # Once done counting down, call the start_timer() again for the corresponding breaks between sessions
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
