82ec37277f01466eb87203dfa4be83b71737ac23
benj-lazaro <benjie.work@gmail.com>
2022-05-02 14:33:50 +0800

    Add functionality that displays the English translation of the current French word

--- day-31-flash-card/flash_card.py
+++ day-31-flash-card/flash_card.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pandas
 import random
 
+# Constant variable(s)
 BACKGROUND_COLOR = "#B1DDC6"
 
 # Read the CSV file as a DataFrame, convert it into a dictionary & orient it as records
@@ -10,21 +11,45 @@ to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    """Pick a random French word along with its English translation from the imported CSV file"""
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    """Pick a random French word from the imported CSV file & store it as a dictionary"""
+    global current_card, flip_timer
 
+    # Cancel/reset the 3 seconds delay timer before picking a new French word
+    window.after_cancel(flip_timer)
+
+    # Pick & display a new French word
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+
+    # Set a new 3 seconds delay timer
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    """Display the equivalent English word of the current French word"""
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+# Stores the current French word
+current_card = {}
 
 # Set up the window
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# Set up the background image
+# Set a 3 seconds delay timer before displaying the English translation of the current French word
+flip_timer = window.after(3000, func=flip_card)
+
+# Set up the canvas
 canvas = Canvas(width=800, height=526)
+card_back_img = PhotoImage(file="images/card_back.png")
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 153, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 264, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
--- day-31-flash-card/readme.md
+++ day-31-flash-card/readme.md
@@ -3,12 +3,10 @@
 
 ### What does it do?
 The Flash card app will:
-* Show the user a random French word on a card that is taken from a deck of cards
-* Ask the user to press either the Cross button (Red) or Check button (Green)
-* The Red button meant that the user doesn't know the equivalent English word
-* Clicking the Red button will place the card back into the deck
-* The Green button meant that the user knows the equivalent English word
-* Clicking the Green button will remove the card from the deck
+* Pick and display a random French word from the imported CSV file as a dictionary
+* Waits for 3 seconds before the card is flipped and displays the equivalent English translation
+* Clicking the Red button will place the French word back into the dictionary
+* Clicking the Green button will remove the card from the dictionary
 
 ### How does it work?
 On the command-line (or terminal), type the following:<br>
