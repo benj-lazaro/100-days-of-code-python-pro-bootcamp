a4af28d75312c1b91b39e0ccbd369973bb252b9c
benj-lazaro <benjie.work@gmail.com>
2022-05-07 22:23:29 +0800

    Day 32 datetime module test code update

--- day-32-birthday-wisher/playground/datetime_test.py
+++ day-32-birthday-wisher/playground/datetime_test.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+week_day_name = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 # Get current system date & time
 now = dt.datetime.now()
 
@@ -7,12 +9,20 @@ now = dt.datetime.now()
 year = now.year
 # Access the current month
 month = now.month
+# Access the current date
+day = now.day
 # Access the current day of the week; NOTE: Day 0 = Monday
 day_of_week = now.weekday()
-
-print(f"Year: {year}")
+# Access current month, day, year, day of the week & its descriptive name
 print(f"Month: {month}")
+print(f"Day: {day}")
+print(f"Year: {year}")
 print(f"Day of the week: {day_of_week}")
+print(f"Today is: {week_day_name[day_of_week]}")
+print("\n")
 
-date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
-print(date_of_birth)
+# Access details of a user-defined data
+date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4, minute=30, second=45)
+print(f"You're born on {date_of_birth}")
+day_of_week = date_of_birth.weekday()
+print(f"That was on {week_day_name[day_of_week]}")
