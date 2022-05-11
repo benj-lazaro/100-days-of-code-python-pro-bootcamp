d1c49d8b0256c10492adc1006b2071c07a5682c9
benj-lazaro <benjie.work@gmail.com>
2022-05-11 17:12:39 +0800

    Day 32 motivational quote, refactor code to move the sendmail functionality into a separate function

--- day-32-birthday-wisher/motivational_quotes/motivational_quotes.py
+++ day-32-birthday-wisher/motivational_quotes/motivational_quotes.py
@@ -8,6 +8,7 @@ SMTP_SERVER = "smtp.gmail.com"
 SENDER_EMAIL = "blank.slate.acct"
 SENDER_EMAIL_PASSWORD = "password12345"
 RECIPIENT_EMAIL = "test.dummy@yahoo.com"
+DAY_TO_SEND = 0     # 0 = Monday, 6 = Sunday
 
 # Global Variable(s)
 email_message = ""
@@ -15,29 +16,18 @@ quote_list = []
 
 
 def read_quote_file():
-    """Open quotes file, pick one per line & save each entry to a list"""
+    """Open file, read the content & save to a list"""
     with open(QUOTES_FILE) as file:
         quotes = file.readlines()
 
-    # Read each quote & strip the \n character at the end of each line
+    # Read content, strip the \n character at the end of each line & save to a list
     for quote in quotes:
         stripped_quote = quote.strip()
         quote_list.append(stripped_quote)
 
 
-# Get the current day of the week
-now = dt.datetime.now()
-today = now.weekday()
-
-# Check if today is Monday (0)
-if today == 0:
-    # Open quote file & pick a random quote
-    read_quote_file()
-    quote_of_the_day = random.choice(quote_list)
-    # Construct email subject & message content
-    email_message = f"Subject: Monday Motivation... \n\n{quote_of_the_day}"
-
-    # Access email account & then send message
+def send_inspirational_msg():
+    """Send Inspiration Message via SMTP"""
     with smtplib.SMTP(SMTP_SERVER) as connection:
         connection.starttls()
         connection.login(user=SENDER_EMAIL, password=SENDER_EMAIL_PASSWORD)
@@ -46,3 +36,19 @@ if today == 0:
             to_addrs=RECIPIENT_EMAIL,
             msg=email_message
         )
+
+
+# Determine the current day of the week
+now = dt.datetime.now()
+today = now.weekday()
+
+# Check if today is the day to send an inspirational message
+if today == DAY_TO_SEND:
+    # Open the file containing quotes
+    read_quote_file()
+    # Pick a random quote from the list
+    quote_of_the_day = random.choice(quote_list)
+    # Construct email subject title & append the inspirational message for the day
+    email_message = f"Subject: Monday Motivation... \n\n{quote_of_the_day}"
+    # Send inspirational message
+    send_inspirational_msg()
