9a4c33630101f799e7ed99ead469cbae81b5795a
benj-lazaro <benjie.work@gmail.com>
2022-05-16 16:51:45 +0800

    Day 34 Refactor code to make the API parameter values easier to change/modify

--- day-34/quizzler/data.py
+++ day-34/quizzler/data.py
@@ -1,7 +1,13 @@
 import requests
 
+# Parameters for the API
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
 # Access the Open Trivia DB API & pass specific parameters
-response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 
 # Get the questions from the received response
