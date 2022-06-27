174b553c4f1c102338749ba4d8f289a46176a333
benj-lazaro <benjie.work@gmail.com>
2022-06-27 22:25:02 +0800

    Day 48 Cookie Clicker bot app initial commit

--- day-48-cookie-clicker/playground/selenium_webdriver_test.py
+++ day-48-cookie-clicker/playground/selenium_webdriver_test.py
@@ -6,3 +6,6 @@ CHROME_DRIVER_PATH = "../chromedriver"
 # Instantiate a Chrome webdriver object
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 driver.get("https://www.amazon.com")
+
+# Close the browser
+driver.quit()
