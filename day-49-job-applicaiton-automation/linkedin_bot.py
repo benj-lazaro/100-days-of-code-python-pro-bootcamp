f9810a5b3bbf9445c1762367bd1b5e33eb367d16
benj-lazaro <benjie.work@gmail.com>
2022-07-20 18:33:43 +0800

    Day 49 LinkedIn Job Application Bot commit

---
+++ day-49-job-applicaiton-automation/linkedin_bot.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+# Constant Variable(s)
+SHORT_WAIT_TIME = 2
+LONG_WAIT_TIME = 10
+
+# Access the LinkedIn website
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.linkedin.com/")
+time.sleep(SHORT_WAIT_TIME)
+
+# Click LinkedIn Sign In button
+sign_in = driver.find_element(By.CSS_SELECTOR, "div .btn-secondary-emphasis")
+sign_in.click()
+time.sleep(SHORT_WAIT_TIME)
+
+# Provide user credentials & then click the sign-in button
+user_name = driver.find_element(By.ID, "username")
+user_name.send_keys("benjamin.lazaro@gmail.com")
+
+user_password = driver.find_element(By.ID, "password")
+user_password.send_keys("P@ssw0rd9014766")
+
+user_login = driver.find_element(By.CSS_SELECTOR, "button.btn__primary--large")
+user_login.click()
+time.sleep(LONG_WAIT_TIME)
+
+# Graceful logout
+user_account = driver.find_element(By.CSS_SELECTOR, "button#ember18")
+user_account.click()
+time.sleep(SHORT_WAIT_TIME)
+
+# Not working... unable to reach the logout link
+user_logout = driver.find_element(By.CSS_SELECTOR, "div#ember20")
+user_logout.click()
+
+time.sleep(LONG_WAIT_TIME)
---
+++ day-49-job-applicaiton-automation/readme.md
@@ -0,0 +1,16 @@
+## LinkedIn Job Application Bot
+* An automated job application bot
+
+### What does it do?
+The bot app will:
+* Launch the Google Chrome browser
+* Access a user's LinkedIn account
+* Access the Job section's Easy Apply feature
+* Fill-in empty fields with corresponding information
+* Click the button Submit Application
+
+### How does it work?
+On the command-line (or terminal), type the following:<br>
+* python3 <b>linkedin_bot.py</b>
+
+<b>NOTE:</b> It is assumed that the <b>Python 3 interpreter</b> had already been installed on your computer.
